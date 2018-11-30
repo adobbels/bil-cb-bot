21e3d2894958e7890f3dac81de83731c0c93c120
Aurelien Dobbels <adobbels@gmail.com>
2018-11-30 15:42:01 +0100

    text changed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,11 @@ include Facebook::Messenger
 # Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'hi, human!')
+  if message.text.include? "Bonjour"
+    message.reply(text: "Hello")
+  elsif message.text.include? "Bonne nuit"
+    message.reply(text: "A demain")
+  else
+    message.reply(text: 'Pas compris')
+  end
 end
