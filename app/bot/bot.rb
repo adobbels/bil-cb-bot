adaf15fad44c74b7587369695692830989b1d13f
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 17:01:29 +0100

    list

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -59,28 +59,72 @@ end
 Bot.on :postback do |postback|
   case postback.payload
   when 'Mastercard'
-    text = '$'
+    text = 'Nice choice 👍!'
   when 'World Mastercard'
-    text = '$$'
+    text = 'Very good choice 👌!'
   when 'World Elite Mastercard'
-    text = '$$$'
+    text = 'You chose the best 👌!'
   end
   postback.reply(
     text: text
   )
+  postback.reply(
+    text: "What is your query about"
+  )
 
   postback.reply(
-    attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: 'Human, do you like me?',
-          buttons: [
-            { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
-            { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
-          ]
-        }
-      }
+  attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: "Traveling",
+                    subtitle:"Use your Mastercard credit card for everyday purchases",
+                    image_url: 'https://images.ctfassets.net/q33z48p65a6w/cpATCb645qYye8gIQK2ay/94b41190786f34f74f5fe4712fb1494f/Vacation_33x.png?fit=thumb&w=1280&q=60',
+                    buttons: [
+                      {
+                        type: "postback",
+                        title: "This is my card",
+                        payload: "Mastercard",
+                      }
+                    ]
+                  },{
+                    title: "Payments",
+                    subtitle:"Give you the flexibility to explore all the places that matter to you",
+                    image_url: 'https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight/image.adaptive.479.high.jpg/1487785073325.jpg',
+                    buttons: [
+                      {
+                        type: "postback",
+                        title: "This is my card",
+                        payload: "World Mastercard",
+                      }
+                    ]
+                  },{
+                    title: "Limit Management",
+                    subtitle:"Give you the flexibility to explore all the places that matter to you",
+                    image_url: 'https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight/image.adaptive.479.high.jpg/1487785073325.jpg',
+                    buttons: [
+                      {
+                        type: "postback",
+                        title: "This is my card",
+                        payload: "World Mastercard",
+                      }
+                    ]
+                  },{
+                    title: "Insurance",
+                    subtitle:"Outstanding purchasing power and top-of-the-line features and benefits",
+                    image_url: 'https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight_0/image.adaptive.479.high.jpg/1487784767161.jpg',
+                    buttons: [
+                      {
+                        type: "postback",
+                        title: "This is my card",
+                        payload: "World Elite Mastercard",
+                      }
+                    ]
+                  }
+                ]
+                }
+              }
   )
 
 
