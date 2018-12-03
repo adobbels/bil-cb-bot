22639134bbf906b2d1d0bb6ce574ac17d7d2ced7
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 16:49:10 +0100

    list

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -67,17 +67,17 @@ Bot.on :postback do |postback|
   end
   postback.reply(
     text: text
-    attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: 'Human, do you like me?',
-          buttons: [
-            { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
-            { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
-          ]
-        }
-      }
+    # attachment: {
+    #     type: 'template',
+    #     payload: {
+    #       template_type: 'button',
+    #       text: 'Human, do you like me?',
+    #       buttons: [
+    #         { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
+    #         { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
+    #       ]
+    #     }
+    #   }
   )
 end
 
