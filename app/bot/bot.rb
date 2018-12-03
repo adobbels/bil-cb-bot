a9d843cf508390bde6bd1bd2328023b8bb49b178
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 11:09:12 +0100

    generic image and choice

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,6 +5,7 @@ include Facebook::Messenger
 # Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
+  message.mark_seen
   message.typing_on
 
 
@@ -23,4 +24,34 @@ message.reply(
       }
     )
 
+
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [{
+            title: 'Is this the right picture?',
+            subtitle: 'Tap a button to answer.',
+            image_url: 'https://www.visaeurope.com/media/images/visalogo73-189.png',
+            buttons: [
+              {
+                type: 'postback',
+                title: 'Yes!',
+                payload: 'yes',
+              },
+              {
+                type: 'postback',
+                title: 'No!',
+                payload: 'no',
+              }
+            ],
+          }]
+        }
+      }
+    }
+
+
 end
+
+
+
