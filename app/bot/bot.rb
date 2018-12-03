de45c051ef1ef177a29f11a729a7f170cffd9a59
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 11:26:51 +0100

    wip generic

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -8,41 +8,46 @@ Bot.on :message do |message|
   message.mark_seen
   message.typing_on
 
-
 message.reply(
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: [{
-            title: 'Is this the right picture?',
-            subtitle: 'Tap a button to answer.',
-            image_url: 'https://www.visaeurope.com/media/images/visalogo73-189.png',
-            buttons: [
-              { type: 'postback', title: 'Yes!', payload: 'yes' },
-              {type: 'postback',title: 'No!',payload: 'no' },
-            ],
-          }]
-        }
-      }
-    )
+  attachment: {
+    type: 'image',
+    payload: {
+      url: 'https://www.visaeurope.com/media/images/visalogo73-189.png'
+    }
+  }
+)
+
+# message.reply(
+#       attachment: {
+#         type: 'template',
+#         payload: {
+#           template_type: 'button',
+#           text: 'What is the purpose of your query?',
+#           buttons: [
+#             { type: 'postback', title: 'Withdraw', payload: 'WITHDRAW' },
+#             { type: 'postback', title: 'Payments', payload: 'PAYMENTS' },
+#             { type: 'postback', title: 'Insurance', payload: 'INSURANCE' },
+#           ]
+#         }
+#       }
+#     )
 
 
-      # attachment: {
-      #   type: 'template',
-      #   payload: {
-      #     template_type: 'generic',
-      #     elements: [{
-      #       title: 'Is this the right picture?',
-      #       subtitle: 'Tap a button to answer.',
-      #       image_url: 'https://www.visaeurope.com/media/images/visalogo73-189.png',
-      #       buttons: [
-      #         { type: 'postback', title: 'Yes!', payload: 'yes' },
-      #         {type: 'postback',title: 'No!',payload: 'no' },
-      #       ],
-      #     }]
-      #   }
-      # }
+#       attachment: {
+#         type: 'template',
+#         payload: {
+#           template_type: 'generic',
+#           elements: [{
+#             title: 'Is this the right picture?',
+#             subtitle: 'Tap a button to answer.',
+#             image_url: 'https://www.visaeurope.com/media/images/visalogo73-189.png',
+#             buttons: [
+#               { type: 'postback', title: 'Yes!', payload: 'yes' },
+#               { type: 'postback',title: 'No!',payload: 'no' },
+#             ],
+#           }]
+#         }
+#       }
 
 
 
