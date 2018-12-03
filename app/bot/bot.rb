4aca959aee61c1580a14b8df3acc77c7ab5523d0
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 17:27:38 +0100

    list

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -127,8 +127,30 @@ Bot.on :postback do |postback|
               }
   )
 
-
 end
 
+
+Bot.on :postback do |postback|
+
+
+  postback.reply(
+    text: "OK - let me check"
+  )
+
+  postback.reply(
+        attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: 'Did human like it?',
+          buttons: [
+            { type: 'postback', title: 'Yes', payload: 'HUMAN_LIKED' },
+            { type: 'postback', title: 'No', payload: 'HUMAN_DISLIKED' }
+          ]
+        }
+      }
+  )
+
+end
 
 
