5ab57f46a308e0f7ae8a8e7ea7575ab4854e47bc
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 11:17:49 +0100

    wip generic

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -13,13 +13,15 @@ message.reply(
       attachment: {
         type: 'template',
         payload: {
-          template_type: 'button',
-          text: 'What is the purpose of your query?',
-          buttons: [
-            { type: 'postback', title: 'Withdraw', payload: 'WITHDRAW' },
-            { type: 'postback', title: 'Payments', payload: 'PAYMENTS' },
-            { type: 'postback', title: 'Insurance', payload: 'INSURANCE' },
-          ]
+          elements: [{
+            title: 'Is this the right picture?',
+            subtitle: 'Tap a button to answer.',
+            image_url: 'https://www.visaeurope.com/media/images/visalogo73-189.png',
+            buttons: [
+              { type: 'postback', title: 'Yes!', payload: 'yes' },
+              {type: 'postback',title: 'No!',payload: 'no' },
+            ],
+          }]
         }
       }
     )
