539f612b3af1ffc015abe5d5ea9c6c3ccc7a1cc5
Aurelien Dobbels <adobbels@gmail.com>
2018-12-03 16:33:53 +0100

    list

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,79 +5,79 @@ include Facebook::Messenger
 # Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 
-Bot.on :message do |message|
-  message.mark_seen
-  message.typing_on
+# Bot.on :message do |message|
+#   message.mark_seen
+#   message.typing_on
 
-message.reply(text: 'hi! Please choose your credit card.')
+# message.reply(text: 'hi! Please choose your credit card.')
 
-message.reply(
-attachment: {
-      type: "template",
-      payload: {
-        template_type: "list",
-        top_element_style: "compact",
-        elements: [
-          {
-            title: "Classic T-Shirt Collection",
-            subtitle: "See all our colors",
-            image_url: "https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight_1/image.adaptive.479.high.jpg/1487872342914.jpg",
-            buttons: [
-              {
-                title: "View",
-                type: "web_url",
-                url: "https://peterssendreceiveapp.ngrok.io/collection",
-                messenger_extensions: true,
-                webview_height_ratio: "tall",
-                fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-              }
-            ]
-          },
-          {
-            title: "Classic White T-Shirt",
-            subtitle: "See all our colors",
-            default_action: {
-              type: "web_url",
-              url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
-              messenger_extensions: false,
-              webview_height_ratio: "tall"
-            }
-          },
-          {
-            title: "Classic Blue T-Shirt",
-            image_url: "https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight_1/image.adaptive.479.high.jpg/1487872342914.jpg",
-            subtitle: "100% Cotton, 200% Comfortable",
-            default_action: {
-              type: "web_url",
-              url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
-              messenger_extensions: true,
-              webview_height_ratio: "tall",
-              fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-            },
-            buttons: [
-              {
-                title: "Shop Now",
-                type: "web_url",
-                url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                messenger_extensions: true,
-                webview_height_ratio: "tall",
-                fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-              }
-            ]
-          }
-        ],
-         buttons: [
-          {
-            title: "View More",
-            type: "postback",
-            payload: "payload"
-          }
-        ]
-      }
-    }
-  }
-)
-end
+# message.reply(
+# attachment: {
+#       type: "template",
+#       payload: {
+#         template_type: "list",
+#         top_element_style: "compact",
+#         elements: [
+#           {
+#             title: "Classic T-Shirt Collection",
+#             subtitle: "See all our colors",
+#             image_url: "https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight_1/image.adaptive.479.high.jpg/1487872342914.jpg",
+#             buttons: [
+#               {
+#                 title: "View",
+#                 type: "web_url",
+#                 url: "https://peterssendreceiveapp.ngrok.io/collection",
+#                 messenger_extensions: true,
+#                 webview_height_ratio: "tall",
+#                 fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+#               }
+#             ]
+#           },
+#           {
+#             title: "Classic White T-Shirt",
+#             subtitle: "See all our colors",
+#             default_action: {
+#               type: "web_url",
+#               url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+#               messenger_extensions: false,
+#               webview_height_ratio: "tall"
+#             }
+#           },
+#           {
+#             title: "Classic Blue T-Shirt",
+#             image_url: "https://www.mastercard.us/en-us/consumers/find-card-products/credit-cards/mastercard/_jcr_content/contentpar/herolight_1/image.adaptive.479.high.jpg/1487872342914.jpg",
+#             subtitle: "100% Cotton, 200% Comfortable",
+#             default_action: {
+#               type: "web_url",
+#               url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+#               messenger_extensions: true,
+#               webview_height_ratio: "tall",
+#               fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+#             },
+#             buttons: [
+#               {
+#                 title: "Shop Now",
+#                 type: "web_url",
+#                 url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+#                 messenger_extensions: true,
+#                 webview_height_ratio: "tall",
+#                 fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+#               }
+#             ]
+#           }
+#         ],
+#          buttons: [
+#           {
+#             title: "View More",
+#             type: "postback",
+#             payload: "payload"
+#           }
+#         ]
+#       }
+#     }
+#   }
+# )
+# end
 
 
 
