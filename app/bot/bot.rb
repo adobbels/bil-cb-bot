cd3fc17ad07713a351871eba1546cfc11db824e6
Aurelien Dobbels <adobbels@gmail.com>
2018-12-04 08:44:12 +0100

    list

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,6 +4,17 @@ include Facebook::Messenger
 
 # Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
+ message:{
+    text: "Here is a quick reply!",
+    quick_replies:[
+      {
+        content_type:"text",
+        title:"Search",
+        payload:"TEST",
+      }
+    ]
+  }
+
 
 Bot.on :message do |message|
   message.mark_seen
@@ -127,29 +138,29 @@ Bot.on :postback do |postback|
               }
   )
 
-  postback.reply(
-    text: "OK - let me check"
-  )
-
-  postback.reply(
-        attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: 'Did human like it?',
-          buttons: [
-            { type: 'postback', title: 'Yes', payload: 'HUMAN_LIKED' },
-            { type: 'postback', title: 'No', payload: 'HUMAN_DISLIKED' }
-          ]
-        }
-      }
-  )
 end
 
 
 # Bot.on :postback do |postback|
 
 
+#   postback.reply(
+#     text: "OK - let me check"
+#   )
+
+#   postback.reply(
+#         attachment: {
+#         type: 'template',
+#         payload: {
+#           template_type: 'button',
+#           text: 'Did human like it?',
+#           buttons: [
+#             { type: 'postback', title: 'Yes', payload: 'HUMAN_LIKED' },
+#             { type: 'postback', title: 'No', payload: 'HUMAN_DISLIKED' }
+#           ]
+#         }
+#       }
+#   )
 
 # end
 
